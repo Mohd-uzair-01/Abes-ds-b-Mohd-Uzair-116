59d08bb9495489e8806b951d5b13b4a4c803453f
Mohd-uzair-01 <mohduzairofficial786@gmali.com>
2025-11-14 14:26:28 +0530

    Frontend

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import Card from './component/Card'
-import Count1 from '../../../ABES-IT-A(2025-26)/frontend/adi/src/components/Count1'
-function App() {
+import { Routes, Route, Link } from 'react-router-dom'
+
+function Home(){
+    return <h1>The is my home page</h1>
+}
+function About(){
+    return <h1>The is my about page</h1>
+}
+
+function Link1() {
   return (
     <div>
-      <Count1 />
+        <nav>
+            <Link to="/">home</Link>
+            <Link to="/about">about</Link>
+        </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+      </Routes>
     </div>
   )
 }
 
-export default App
+export default Link1
